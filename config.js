21d5a72097ebcd30c2a5ba1c650832d1693b2968
dlawhdgus <212whdgus@gmail.com>
2022-10-07 11:46:02 +0900

    add port

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const config = {
-    MONGODB_CONNECT_STRING: "mongodb+srv://dlawhdgus:dlawhdgus1111@cluster0.aydj8l6.mongodb.net/?retryWrites=true&w=majority"
+    MONGODB_CONNECT_STRING: "mongodb+srv://dlawhdgus:dlawhdgus1111@cluster0.aydj8l6.mongodb.net/?retryWrites=true&w=majority",
+    port: 3000
 }
 module.exports = config
