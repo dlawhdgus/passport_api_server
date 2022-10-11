42effad518442a0eec26caf6b6a4b1be6e770461
dlawhdgus <212whdgus@gmail.com>
2022-10-11 13:42:37 +0900

    connect mongoose

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const routes = require('./api/index')
 const config = require('./config')
-const { DBconnect } = require('./db_connect')
+const { dbconnect } = require('./db_connect')
 
 app.use(express.json())
 app.use('/api', routes)
 
 app.listen(config.port, () => {
     console.log('server on!!')
-    DBconnect()
+    dbconnect
 })
