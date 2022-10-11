116e926195eb0baa9a5ba5ce04ee8c9823131510
dlawhdgus <212whdgus@gmail.com>
2022-10-11 13:43:13 +0900

    change to mongoose

--- db_connect.js
+++ db_connect.js
@@ -1,16 +1,17 @@
-const MongoClient = require('mongodb').MongoClient
 const config = require('./config')
+const mongoose = require('mongoose')
 
-exports.DBconnect = () => {
-    MongoClient.connect(config.MONGODB_CONNECT_STRING, (err, db) => {
-        if (err) throw err
-        else {
-            let dbo = db.db("db")
-            let ArticleColl = dbo.collection("article")
-            let AuthColl = dbo.collection('auth')
-            exports.ArticleColl = ArticleColl
-            exports.AuthColl = AuthColl
-            console.log('DB connected!!')
-        }
-    })
-}
+mongoose.connect(config.MONGODB_CONNECT_STRING,
+    {
+        dbName: 'db'
+    }
+)
+
+const db = mongoose.connection
+exports.dbconnect = db.once("open", () => {
+    console.log('DB Connected!!')
+})
+db.on("error", (err) => {
+    console.log(err)
+})
+
