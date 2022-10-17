3bf5be6622701e76d0f3f26dc83ddf85eb77aa02
dlawhdgus <212whdgus@gmail.com>
2022-10-17 10:49:11 +0900

    middleware

--- routes/api/auth/controller.js
+++ routes/api/auth/controller.js
@@ -1,48 +1,10 @@
 //토큰을 발급해주는 곳
 const LocalStrategy = require('passport-local').Strategy
 const { connection } = require('mongoose')
-const passport = require('passport')
 const AuthColl = connection.collection('auth')
 const jwt = require('jsonwebtoken')
-const CryptoJS = require('crypto-js')
+const crypto = require('./crypto')
 const config = require('../../../config')
-
-const encoding = (password) => {
-    return CryptoJS.AES.encrypt(JSON.stringify(password), config.SECRET_KEY).toString()
-}
-const decoding = (password) => {
-    const bytes = CryptoJS.AES.decrypt(password,config.SECRET_KEY)
-    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
-}
-
-passport.use(new LocalStrategy(
-    {
-        usernameField : 'id',
-        passwordField : 'pw'
-    },
-    function (username, password, done) {
-        AuthColl.findOne({id : username})
-        .then(result => {
-            if(password === decoding(result.pw)) {
-                AuthColl.findOne({id: username, pw: result.pw})
-                .then(user => {
-                    if(user) {
-                        return done(null, user)
-                    }
-                    else {
-                        return done(null, false, {message : 'Incorrect password'})
-                    }
-                })
-                .catch(e => {
-                    console.log('Internal Server Error')
-                })
-            }
-            else return done(err)
-        })
-        .catch(e => {if(e) throw e})
-    }
-))
-
 
 exports.SignUp = (req, res) => {
     const { id, pw } = req.body
@@ -56,7 +18,7 @@ exports.SignUp = (req, res) => {
         if(result) res.status(400).send('id가 존재합니다')
         else {
             SignUpFilter.id = id
-            SignUpFilter.pw = encoding(pw)
+            SignUpFilter.pw = crypto.encoding(pw)
             AuthColl.insertOne(SignUpFilter)
             .then(res.send('success'))
             .catch(e => {if(e) throw e})
@@ -75,7 +37,8 @@ exports.SignIn = (req, res) => {
                 expiresIn: '1h'
             })
             res.cookie('user',token)
-            res.send(token)
+            res.send(`login success
+${token}`)
         }
     })
     .catch(e => {if(e) throw e})
