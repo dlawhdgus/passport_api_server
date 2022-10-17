bf45b9f4ff0131c4ad37ecb84e0abcc4cfadb2b5
dlawhdgus <212whdgus@gmail.com>
2022-10-17 10:48:26 +0900

    crypto

---
+++ routes/api/auth/crypto.js
@@ -0,0 +1,11 @@
+const CryptoJS = require('crypto-js')
+const config = require('../../../config')
+
+exports.encoding = (password) => {
+    return CryptoJS.AES.encrypt(JSON.stringify(password), config.SECRET_KEY).toString()
+}
+
+exports.decoding = (password) => {
+    const bytes = CryptoJS.AES.decrypt(password,config.SECRET_KEY)
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+}
