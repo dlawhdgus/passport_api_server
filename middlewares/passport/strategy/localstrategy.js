f0d6c45712f2c2292d14011d35c51a69ae103be6
dlawhdgus <212whdgus@gmail.com>
2022-10-17 10:48:46 +0900

    passport

--- middlewares/passport/strategy/localstrategy.js
+++ middlewares/passport/strategy/localstrategy.js
@@ -2,29 +2,37 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const { connection } = require('mongoose')
 const AuthColl = connection.collection('auth')
+const crypto = require('../../../routes/api/auth/crypto')
 
-const auth = () => {
+module.exports = () => {
     passport.use(new LocalStrategy(
         {
             usernameField : 'id',
             passwordField : 'pw'
         },
         function (username, password, done) {
-            console.log('LocalStrategy',username, password)
-            AuthColl.findOne({id: username, pw: password})
-            .then(user => {
-                if(user) {
-                    return done(null, user)
+            AuthColl.findOne({id : username})
+            .then(result => {
+                if(result) {
+                    if(password === crypto.decoding(result.pw)) {
+                        AuthColl.findOne({id: username, pw: result.pw})
+                        .then(user => {
+                            if(user) {
+                                return done(null, user)
+                            }
+                            else {
+                                return done(null, false, {message : 'Incorrect password'})
+                            }
+                        })
+                        .catch(e => {
+                            console.log('Internal Server Error')
+                        })
+                    }
+                    else return done(null, false)
                 }
-                else {
-                    return done(null, false, {message : 'Incorrect password'})
-                }
+                else return done(null, false, {message : 'Bad Request'})
             })
-            .catch(e => {
-                console.log('Internal Server Error')
-            })
+            .catch(e => {if(e) throw e})
         }
     ))
 }
-
-module.exports = auth
