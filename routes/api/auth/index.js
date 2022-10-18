cb85a9dd0efc24135d964b32fdf8f4ad21d6f642
dlawhdgus <212whdgus@gmail.com>
2022-10-18 16:09:43 +0900

    add -

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const controller = require('./controller')
 const passport = require('passport')
 
-router.post('/signup', controller.SignUp)
-router.post('/signin', passport.authenticate('local', { session: false }), controller.SignIn)
+router.post('/sign-up', controller.SignUp)
+router.post('/sign-in', passport.authenticate('local', { session: false }), controller.SignIn)
 module.exports = router
